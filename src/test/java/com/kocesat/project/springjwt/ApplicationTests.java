3c90baff3edafe8d2857863a880872d9dda3b6b2
kocesat <kocesat@gmail.com>
2022-11-15 21:50:02 +0300

    first ever commit

---
+++ src/test/java/com/kocesat/project/springjwt/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kocesat.project.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
