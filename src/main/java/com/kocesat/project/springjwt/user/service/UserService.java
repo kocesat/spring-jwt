e64d0ef855f57d67e39cb74a1799ed80ea9eef54
kocesat <kocesat@gmail.com>
2023-01-04 01:59:16 +0300

    resolve conflict

--- src/main/java/com/kocesat/project/springjwt/user/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.kocesat.project.springjwt.user.service;
-
-import com.kocesat.project.springjwt.user.model.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    User getByUsername(String username);
-}
